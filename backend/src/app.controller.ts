cc5717a3184603d23c449528e142bdc9f8a24f3c
Guilherme Souza <gui.edu0380@gmail.com>
2026-02-11 14:15:24 -0300

    adicionando login na plataforma da impressora

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,17 +1,36 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import qs from 'qs';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
 
+interface AxiosCookiesConfig extends AxiosRequestConfig {
+  jar: CookieJar
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  private jar = new CookieJar();
+
+  private client = wrapper(axios.create({
+    jar: this.jar,
+    withCredentials: true
+  }));
+
   @Post('login')
   async login() {
-    const login = await axios.post('http://192.168.1.75/checkLogin.cgi',
-      {
+    const login = await this.client.post('http://192.168.1.75/checkLogin.cgi',
+      qs.stringify({
         i0023: '1643',
         i0025: '1643'
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-unrlencoded"
+        },
+        withCredentials: true
       }
     )
     return login.data
