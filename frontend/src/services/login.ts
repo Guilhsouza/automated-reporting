d4c2d3f5d53fa2486d276f776d1cbbe7bd52f799
Guilherme Souza <gui.edu0380@gmail.com>
2026-02-12 15:03:12 -0300

    alternado caminho da rota

--- frontend/src/services/login.ts
+++ frontend/src/services/login.ts
@@ -2,7 +2,7 @@ import { api } from './api'
 
 export default function login() {
     try {
-        const response = api.post('/login')
+        const response = api.post('/loginAndGetData')
         return response
     } catch (e) {
         throw e
