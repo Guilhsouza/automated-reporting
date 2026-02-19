837958d35a5d3934ef6a02f7569fdc970abebf6d
Guilherme Souza <gui.edu0380@gmail.com>
2026-02-19 15:02:19 -0300

    adicionando config para trabalhar com dotenv

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // não precisa importar em todo módulo
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
